5a11ea54267309246da06ca4c8007b17d1187502
Adrian Perez <adrianp1299@gmail.com>
2024-01-15 20:55:09 -0700

    Finished Server And structure components

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,21 @@
 // Write your "actions" router here!
+
+router.get('/api/actions', (req, res, next) =>{
+
+});
+
+router.get('/api/actions/:id', (req, res, next) =>{
+
+});
+
+router.post('/api/actions', (req, res, next) =>{
+
+});
+
+router.put('/api/actions/:id', (req, res, next) =>{
+
+});
+
+router.delete('/api/actions/:id', async (req, res, next) =>{
+
+});
--- api/server.js
+++ api/server.js
@@ -6,4 +6,28 @@ const server = express();
 // Build your projects router in /api/projects/projects-router.js
 // Do NOT `server.listen()` inside this file!
 
+router.get('/api/projects', (req, res, next) =>{
+
+});
+
+router.get('/api/projects/:id', (req, res, next) =>{
+
+});
+
+router.post('/api/projects', (req, res, next) =>{
+
+});
+
+router.put('/api/projects/:id', (req, res, next) =>{
+
+});
+
+router.delete('/api/projects/:id', async (req, res, next) =>{
+
+});
+
+router.get('/api/projects/:id/actions', (req, res, next) =>{
+
+});
+
 module.exports = server;
