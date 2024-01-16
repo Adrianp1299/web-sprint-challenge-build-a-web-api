4a0206fcbb22f76b7bddee68fdb76f43da14662a
Adrian Perez <adrianp1299@gmail.com>
2024-01-15 21:35:32 -0700

    server finished

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,21 +1,24 @@
 // Write your "actions" router here!
+const express = require('express')
 
-router.get('/api/actions', (req, res, next) =>{
+// router.get('/api/actions', (req, res, next) =>{
 
-});
+// });
 
-router.get('/api/actions/:id', (req, res, next) =>{
+// router.get('/api/actions/:id', (req, res, next) =>{
 
-});
+// });
 
-router.post('/api/actions', (req, res, next) =>{
+// router.post('/api/actions', (req, res, next) =>{
 
-});
+// });
 
-router.put('/api/actions/:id', (req, res, next) =>{
+// router.put('/api/actions/:id', (req, res, next) =>{
 
-});
+// });
 
-router.delete('/api/actions/:id', async (req, res, next) =>{
+// router.delete('/api/actions/:id', async (req, res, next) =>{
 
-});
+// });
+
+module.exports = router
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,28 @@
 // Write your "projects" router here!
+const express = require('express')
+
+// router.get('/api/projects', (req, res, next) =>{
+
+// });
+
+// router.get('/api/projects/:id', (req, res, next) =>{
+
+// });
+
+// router.post('/api/projects', (req, res, next) =>{
+
+// });
+
+// router.put('/api/projects/:id', (req, res, next) =>{
+
+// });
+
+// router.delete('/api/projects/:id', async (req, res, next) =>{
+
+// });
+
+// router.get('/api/projects/:id/actions', (req, res, next) =>{
+
+// });
+
+module.exports = router
